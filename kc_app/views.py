c7d664ee9c9eb9e84b9d0f8da6d2eb4bdfce1b6b
Arnie <59781783+Barani2396@users.noreply.github.com>
2020-04-04 01:16:22 -0400

    initial commit

--- kc_app/views.py
+++ kc_app/views.py
@@ -3,11 +3,11 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def home(request):
-    return render(request, 'kc_app/home.html')
+def home (request):
+    return render(request, 'kc_app/home.html', {'title': 'Home'})
 
 def cat_services(request):
-    return render(request, 'kc_app/cat_services.html')
+    return render(request, 'kc_app/cat_services.html', {'title': 'Services'})
 
 def about(request):
-    return render(request, 'kc_app/about.html')
+    return render(request, 'kc_app/about.html', {'title': 'About'})
