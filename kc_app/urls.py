12be973f4f262870e15a4184baaf46a3f02c86b1
Arnie <59781783+Barani2396@users.noreply.github.com>
2020-04-05 14:48:06 -0400

    initial commit

--- kc_app/urls.py
+++ kc_app/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('services/', views.cat_services, name="kc_app-cat_services"),
     path('order_online/', views.order_online, name="kc_app-order_online"),
     path('reviews/', views.reviews, name="kc_app-reviews"),
+    path('contact_us/', views.contact_us, name="kc_app-contact"),
     path('donate/', views.donate, name="kc_app-donate"),
 ]
--- kc_app/views.py
+++ kc_app/views.py
@@ -3,20 +3,49 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def home (request):
+reviews_d = [
+    {
+        'customer': 'Joshua',
+        'r_title': 'Review 1',
+        'content': 'Service is literally good.',
+        'date': 'March 23, 2020'
+    },
+
+    {
+        'customer': 'Sid',
+        'r_title': 'Review 2',
+        'content': 'Service is ok.',
+        'date': 'January 05, 2020',
+    }
+
+]
+
+
+def home(request):
     return render(request, 'kc_app/home.html', {'title': 'Home'})
+
 
 def cat_services(request):
     return render(request, 'kc_app/services.html', {'title': 'Services'})
 
+
 def order_online(request):
     return render(request, 'kc_app/order_online.html', {'title': 'Online Order'})
 
+
 def reviews(request):
-    return render(request, 'kc_app/reviews.html', {'title': 'Reviews'})
+    contexts = {
+        'reviews_d': reviews_d, 
+        'title': 'Reviews',
+    }
+    return render(request, 'kc_app/reviews.html', contexts)
+
+def contact_us(request):
+    return render(request, 'kc_app/contact.html', {'title': 'Contact us'})
 
 def donate(request):
     return render(request, 'kc_app/donate.html', {'title': 'Donate'})
+
 
 def about(request):
     return render(request, 'kc_app/about.html', {'title': 'About'})
